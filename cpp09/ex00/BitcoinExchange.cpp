8c8374347b375fec26b819d59536e3c72414ae35
SiNoova <alaekoutate1337@outlook.fr>
2025-11-08 03:12:27 +0100

    a

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -33,11 +33,30 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
 	return *this;
 }
 
+void parse_date(std::string date)
+{
+
+}
+
 void BitcoinExchange::ProcessInput(const char *input_path)
 {
 	std::ifstream file(input_path);
 	if (!file.is_open())
 		throw std::runtime_error("Error: could not open input file.");
+
+	std::string line;
+	getline(file, line);
+
+	while (getline(file, line))
+	{
+		std::stringstream ss(line);
+		std::string date, rateStr;
+		if (getline(ss, date, '|') && getline(ss, rateStr))
+		{
+			float rate = atof(rateStr.c_str());
+			databise[date] = rate;
+		}
+	}
 }
 
 
